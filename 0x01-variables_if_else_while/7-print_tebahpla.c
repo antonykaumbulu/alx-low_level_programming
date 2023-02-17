607b428469459ff842fa087aad621d1797756e56
Antony MK <antonykaumbulu@gmail.com>
2023-02-17 05:25:36 -0800

    Reverse lowercase alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Lowercase aphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'z'; letter >= 'a'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
