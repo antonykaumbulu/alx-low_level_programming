ca1f680d586ceca94bc4af6f27799fb918bec81c
Antony MK <antonykaumbulu@gmail.com>
2023-02-17 07:01:29 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,14 @@ int main(void)
 	int num;
 
 	for (num = 48; num < 58; num++)
+	{
 		putchar(num);
 		if (num != 57)
+		{
 			putchar(',');
 			putchar(' ');
+		}
+	}
 	putchar("\n");
 	return (0);
 }
