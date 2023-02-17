9a05c79fd0209be71e28a611d8740665c9c7bf67
Antony MK <antonykaumbulu@gmail.com>
2023-02-17 06:36:55 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,9 @@
 int main(void)
 {
 	int num;
-	int num2;
 
-	for (num2 = 0; num2 <= 9; num2++)
-		putchar(num2);
+	for (num = 0; num < 10; num++)
+		putchar(num);
 	for (num = 'a'; num <= 'f'; num++)
 		putchar(num);
 	putchar('\n');
