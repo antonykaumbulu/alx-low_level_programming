a2609d4362c35e9849d6d61c8302323989f1a742
Antony MK <antonykaumbulu@gmail.com>
2023-02-17 02:58:15 -0800

    Alphabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-        int letter;
+	int letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-		for (letter = 'A'; letter <= 'Z'; letter++)
-			putchar(letter);
-        }
-        putchar('\n');
-        return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
 }
