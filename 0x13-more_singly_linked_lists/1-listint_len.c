378f7cc2358197b0f7772259c092091185c8e9db
Antony MK <antonykaumbulu@gmail.com>
2023-04-03 13:49:59 -0700

    Lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in a list
+ * @h: head of the list
+ *
+ * return: Returns number of elemments
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n_nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n_nodes++;
+	}
+	return (n_nodes);
+}
